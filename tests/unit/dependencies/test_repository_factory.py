b9c79b17e80f44d8a839ea2c299f944096e06060
Ruben35 <ruben111416@gmail.com>
2025-07-23 19:15:03 -0600

    refactor: :sparkles: New dependencies

---
+++ tests/unit/dependencies/test_repository_factory.py
@@ -0,0 +1,15 @@
+from unittest.mock import patch
+
+from app.dependencies import RepositoryFactory
+from app.repositories import UserRepository
+
+
+@patch("app.dependencies.repository_factory.get_db")
+class TestRepositoryFactory:
+
+    def test_user_repository(self, mock_get_db):
+        repository_generator = RepositoryFactory.user_repository(mock_get_db)
+        repository = next(repository_generator)
+
+        assert isinstance(repository, UserRepository)
+        assert repository.db == mock_get_db
