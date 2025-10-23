2b9b8a05cd89555862272a99351e2c15f48ccb55
Ruben35 <ruben111416@gmail.com>
2025-10-23 17:41:20 -0600

    fix: :bug: Fix session not closed properly

--- app/database/session.py
+++ app/database/session.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from app.database.core import engine
 
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
